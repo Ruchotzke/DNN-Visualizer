c790d704bbc2a5ca1806f1a1b5ce9cb7d5f338d4
Ethan Ruchotzke <35568374+Ruchotzke@users.noreply.github.com>
2021-12-03 16:12:53 -0600

    Connections Working

--- DNN-Visualizer/Assets/LayerLayout.cs
+++ DNN-Visualizer/Assets/LayerLayout.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UI.Extensions;
 
 public class LayerLayout : MonoBehaviour
@@ -8,7 +9,7 @@ public class LayerLayout : MonoBehaviour
     #region SERIALIZED
     [Header("Prefabs")]
     public GameObject pf_Layer;
-    public GameObject pf_Neuron;
+    public Neuron pf_Neuron;
     public UILineRenderer pf_LineRenderer;
 
     [Header("Containers")]
@@ -18,7 +19,7 @@ public class LayerLayout : MonoBehaviour
 
     #region PROPERTIES
     DNN dnn;
-    List<List<Transform>> neurons = new List<List<Transform>>();
+    List<List<Neuron>> neurons = new List<List<Neuron>>();
     #endregion
 
     private void Awake()
@@ -36,12 +37,14 @@ public class LayerLayout : MonoBehaviour
         /* First generate an input layer */
         Transform inputLayer = Instantiate(pf_Layer, LayerContainer).transform;
         inputLayer.name = "Input layer";
-        neurons.Add(new List<Transform>());
+        neurons.Add(new List<Neuron>());
         for (int i = 0; i < dnn.layers[0].inputSize; i++)
         {
             var neuron = Instantiate(pf_Neuron, inputLayer);
+            neuron.layer = null;
+            neuron.index = i;
             neuron.name = "I" + i;
-            neurons[0].Add(neuron.transform);
+            neurons[0].Add(neuron);
         }
 
         /* Next generate all hidden layers */
@@ -51,29 +54,42 @@ public class LayerLayout : MonoBehaviour
             /* Generate a new layer */
             Transform layerInstance = Instantiate(pf_Layer, LayerContainer).transform;
             layerInstance.name = "Layer " + layerCount++;
-            neurons.Add(new List<Transform>());
+            neurons.Add(new List<Neuron>());
 
             /* Add neurons for this layer */
             for (int i = 0; i < layer.size; i++)
             {
                 var neuron = Instantiate(pf_Neuron, layerInstance);
+                neuron.layer = layer;
+                neuron.index = i;
                 neuron.name = "N" + i;
-                neurons[neurons.Count - 1].Add(neuron.transform);
+                neurons[neurons.Count - 1].Add(neuron);
             }
+
+            /* Unity UI quirk - update the layout positions before the end of the frame */
+            LayoutRebuilder.ForceRebuildLayoutImmediate(LayerContainer.GetComponent<RectTransform>());
+            LayoutRebuilder.ForceRebuildLayoutImmediate(layerInstance.GetComponent<RectTransform>());
         }
 
         /* Finally, densely connect all neurons */
-        for(int i = 0; i < neurons.Count - 1; i++)
+        for (int i = 0; i < neurons.Count - 1; i++)
         {
             /* For each layer, connect all outgoing neurons to the next layer's incoming neurons */
             for(int outNeuron = 0; outNeuron < neurons[i].Count; outNeuron++)
             {
                 for(int inNeuron = 0; inNeuron < neurons[i+1].Count; inNeuron++)
                 {
-                    Debug.Log("LINE: " + neurons[i][outNeuron].GetComponent<RectTransform>().anchoredPosition + ", " + neurons[i + 1][inNeuron].GetComponent<RectTransform>().anchoredPosition + ": " + neurons[i][outNeuron].name + ", " + neurons[i+1][inNeuron].name);
+                    Neuron startNeuron = neurons[i][outNeuron];
+                    Neuron endNeuron = neurons[i + 1][inNeuron];
+                    Vector3 start = startNeuron.transform.position;
+                    Vector3 end = endNeuron.transform.position;
+
                     var lr = Instantiate(pf_LineRenderer, LineRendererContainer);
-                    lr.Points = new Vector2[2] { neurons[i][outNeuron].GetComponent<RectTransform>().anchoredPosition, neurons[i + 1][inNeuron].GetComponent<RectTransform>().anchoredPosition };
+                    
+                    lr.Points = new Vector2[2] { start, end };
                     lr.SetAllDirty();
+
+                    neurons[i][outNeuron].AddLine(lr);
                 }
             }
         }
---
+++ DNN-Visualizer/Assets/Neuron.cs
@@ -0,0 +1,37 @@
+using DNNElements;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI.Extensions;
+
+public class Neuron : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private List<UILineRenderer> outputConnections = new List<UILineRenderer>();
+    private List<Color> originalColors = new List<Color>();
+
+    public Layer layer;
+    public int index;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        foreach(var line in outputConnections)
+        {
+            line.color = Color.red;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        for(int i = 0; i < outputConnections.Count; i++)
+        {
+            outputConnections[i].color = originalColors[i];
+        }
+    }
+
+    public void AddLine(UILineRenderer line)
+    {
+        outputConnections.Add(line);
+        originalColors.Add(line.color);
+    }
+}
